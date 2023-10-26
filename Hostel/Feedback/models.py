5012708112b725a29404c5b1cb6e61c727d27af5
DanielDante12 <ofwonodani12@gmail.com>
2023-10-26 08:57:15 +0300

    Add models.py

---
+++ Hostel/Feedback/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+class Hostel(models.Model):
+    name=models.CharField(max_length=50)
+    address=models.CharField(max_length=50)
+    telno=models.CharField(max_length=15)
+    email=models.CharField(max_length=30)
+    status=models.CharField(max_length=20, default="not approved")
+    image=models.ImageField(upload_to='static/hostel')
+    manager=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+class Feedback(models.Model):
+    rating=models.DecimalField(max_digits=5, decimal_places=2)
+    comment=models.CharField(max_length=200)
+    date=models.DateTimeField(auto_now_add=True)
+    student=models.ForeignKey(User,on_delete=models.CASCADE)
+    hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.comment
+
+
+class Payment(models.Model):
+    amount=models.DecimalField(max_digits=10,decimal_places=2)
+    paymentDate=models.DateField()
+    paymentMethod=models.CharField(max_length=20)
+
+class Reservation(models.Model):
+    checkInDate=models.DateField()
+    checkOutDate=models.DateField()
+    student=models.ForeignKey(User, on_delete=models.CASCADE)
+    payment=models.ForeignKey(Payment, on_delete=models.CASCADE)
+
+class Room(models.Model):
+    room_number=models.CharField(max_length=6, unique=True)
+    occupants = models.DecimalField(max_digits=5, decimal_places=0, default=0)
+    hostel=models.ForeignKey(Hostel, on_delete=models.CASCADE)
+    reservation=models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
+
+    def __str__(self):
+        return self.room_number
+# Create your models here.
