22da23efd36d17358ff9490807cbfa40603d66ea
DanielDante12 <ofwonodani12@gmail.com>
2023-10-26 09:03:51 +0300

    end points created for specific models

---
+++ Hostel/Feedback/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Hostel, Feedback, Room
+admin.site.register(Hostel)
+admin.site.register(Feedback)
+admin.site.register(Room)
+# Register your models here.
