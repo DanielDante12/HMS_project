eb006143d1897571f5c1be1200d7f0783faecdef
DanielDante12 <ofwonodani12@gmail.com>
2023-10-26 13:32:01 +0300

    Models.py file modified

--- Hostel/Feedback/models.py
+++ Hostel/Feedback/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-
+savepath = "static/rooms/"
 class Hostel(models.Model):
     name=models.CharField(max_length=50)
     address=models.CharField(max_length=50)
@@ -23,12 +23,23 @@ class Feedback(models.Model):
 
     def __str__(self):
         return self.comment
-
+class Room(models.Model):
+    room_number=models.CharField(max_length=6, unique=True)
+    occupants = models.DecimalField(max_digits=5, decimal_places=0, default=0)
+    hostel=models.ForeignKey(Hostel, on_delete=models.CASCADE)
+    reservation=models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
+    image0 = models.ImageField(upload_to=savepath, null=True, blank = True)
+    image1 = models.ImageField(upload_to=savepath, null=True, blank = True)
+    image2 = models.ImageField(upload_to=savepath, null=True, blank = True)
+    image3 = models.ImageField(upload_to=savepath, null=True, blank = True)
+    def __str__(self):
+        return self.room_number
 
 class Payment(models.Model):
     amount=models.DecimalField(max_digits=10,decimal_places=2)
-    paymentDate=models.DateField()
+    paymentDate=models.DateField(auto_now_add=True)
     paymentMethod=models.CharField(max_length=20)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
 class Reservation(models.Model):
     checkInDate=models.DateField()
@@ -36,12 +47,5 @@ class Reservation(models.Model):
     student=models.ForeignKey(User, on_delete=models.CASCADE)
     payment=models.ForeignKey(Payment, on_delete=models.CASCADE)
 
-class Room(models.Model):
-    room_number=models.CharField(max_length=6, unique=True)
-    occupants = models.DecimalField(max_digits=5, decimal_places=0, default=0)
-    hostel=models.ForeignKey(Hostel, on_delete=models.CASCADE)
-    reservation=models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
 
-    def __str__(self):
-        return self.room_number
 # Create your models here.
